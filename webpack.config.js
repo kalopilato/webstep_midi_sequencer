f9cef58baeae7b67c8e8975af2cc248e779756d6
Kalo Pilato <mista.kay@gmail.com>
2017-04-05 16:54:10 +1200

    Added module paths to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,12 @@ module.exports = {
   },
   resolve: {
     root: __dirname,
+    modulesDirectories: [
+      'node_modules',
+      './app/components'
+    ],
     alias: {
+      app: 'app',
       applicationStyles: 'app/styles/app.scss',
     },
     extensions: ['', '.js', '.jsx']
