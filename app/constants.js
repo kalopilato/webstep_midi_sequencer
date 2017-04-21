3ff4ff3815ee4319acb857c3bd4bd742e03f358c
Kalo Pilato <mista.kay@gmail.com>
2017-04-21 16:27:02 +1200

    Removed duplicate scale

--- app/constants.js
+++ app/constants.js
@@ -17,7 +17,6 @@ export const STEP_VALUES = ['1/4',
                             '1/32']
 
 export const SCALES = {
-  NATURAL_MINOR:      [0, 2, 3, 5, 7, 8, 10, 12],
   HARMONIC_MINOR:     [0, 2, 3, 5, 7, 8, 11, 12],
   HUNGARIAN_MINOR:    [0, 2, 3, 6, 7, 8, 11, 12],
   MAJOR:              [0, 2, 4, 5, 7, 9, 11, 12],
